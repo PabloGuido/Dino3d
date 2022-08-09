2c1a5fa8a2f76d8055dd3495d76fc6a168de9e2f
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-08-09 16:23:06 -0300

    Avances en la detección de colisiones.

---
+++ Assets/scripts/playerColision.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class playerColision : MonoBehaviour
+{
+    public playerScr objectWithScript;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //Output the Collider's GameObject's name
+        // Debug.Log(collisionInfo.gameObject.tag);
+        Debug.Log(objectWithScript.numero);
+    }
+
+
+
+
+
+}
---
+++ Assets/scripts/playerScr.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScr : MonoBehaviour
+{
+    [HideInInspector][SerializeField] public float numero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numero = 6234.234234f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
