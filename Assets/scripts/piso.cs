edfe450504cedbb254c6b87717cc79e50b039676
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-08-09 12:33:59 -0300

    Finalmente se agregó el sprite 2d del dino. A punto de ver el tema del salto.

--- Assets/scripts/piso.cs
+++ Assets/scripts/piso.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class piso : MonoBehaviour
 {
-    Mesh m_Collider;
-    Vector3 m_Max;
+
+    [SerializeField] private List<GameObject> objetos;
+
     void Start()
     {
-        m_Collider = GetComponent<MeshFilter>().mesh;
-        m_Max = m_Collider.bounds.size;
+        GameObject piso01 = GameObject.Find("piso01");
+        GameObject piso02 = GameObject.Find("piso02");
 
-        Debug.Log(m_Max);
+        objetos.Add(piso01);
+        objetos.Add(piso02);
+        // Debug.Log(objetos);
+        foreach(GameObject obj in objetos)
+        {
+            Debug.Log(obj.transform.position.x);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3 (gameObject.transform.position.x-0.055f, gameObject.transform.position.y,gameObject.transform.position.z);
-        if (gameObject.transform.position.x <= -50f){
-            transform.position = new Vector3(50f, 0, 0);
+        // if (gameObject.transform.position.x <= -40f){
+        //     transform.position = new Vector3((gameObject.transform.position.x+otroPiso.position.x)+80f-0.03f,0,0);
+        // }
+        // else{
+        //     transform.position = new Vector3 (gameObject.transform.position.x-0.1f, gameObject.transform.position.y,gameObject.transform.position.z);
+        //  }
+        foreach(GameObject obj in objetos)
+        {
+            if (obj.transform.position.x <= -40){
+                obj.transform.position = new Vector3(39.95f,0,0);
+            }
+            obj.transform.position = new Vector3(obj.transform.position.x-0.05f,0,0);
+            // Debug.Log(obj.transform.position.x);
         }
+
     }
 }
