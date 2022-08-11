1f03a84b63fd8e2065882921eec7663569a0b532
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-08-11 15:07:05 -0300

    Investigando scriptable objects.

--- Assets/obstaculos/obstaculo.cs
+++ Assets/obstaculos/obstaculo.cs
@@ -7,7 +7,8 @@ public class obstaculo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Cubo creado.");
+        // Debug.Log("Cubo creado.");
+        
     }
 
     // Update is called once per frame
--- Assets/scripts/piso.cs
+++ Assets/scripts/piso.cs
@@ -8,6 +8,7 @@ public class piso : MonoBehaviour
     [SerializeField] private List<GameObject> objetos;
     GameObject piso01;
     GameObject piso02;
+    bool mover = false;
     void Start()
     {
         piso01 = GameObject.Find("piso01");
@@ -22,31 +23,23 @@ public class piso : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (gameObject.transform.position.x <= -40f){
-        //     transform.position = new Vector3((gameObject.transform.position.x+otroPiso.position.x)+80f-0.03f,0,0);
-        // }
-        // else{
-        //     transform.position = new Vector3 (gameObject.transform.position.x-0.1f, gameObject.transform.position.y,gameObject.transform.position.z);
-        //  }
-        foreach(GameObject obj in objetos)
-        {
+        if (mover){
+            foreach(GameObject obj in objetos)
+            {
 
-            if (obj.transform.position.x <= -40){
-                // obj.transform.position = new Vector3(40f,0,0);
-                if (obj.name == "piso01"){
-                    obj.transform.position = new Vector3(piso02.transform.position.x+40f ,0,0);
+                if (obj.transform.position.x <= -40){
+                    // obj.transform.position = new Vector3(40f,0,0);
+                    if (obj.name == "piso01"){
+                        obj.transform.position = new Vector3(piso02.transform.position.x+40f ,0,0);
+                    }
+                    else{
+                        obj.transform.position = new Vector3(piso01.transform.position.x+40f ,0,0);
+                    }
                 }
-                else{
-                    obj.transform.position = new Vector3(piso01.transform.position.x+40f ,0,0);
-                }
+                var vec = new Vector3(-12.5f,0,0);
+                obj.transform.Translate(vec * Time.deltaTime, Space.World);
+                // Antes estaba usando solo el transform y moviendo pero se cambió a Translate.
             }
-            var vec = new Vector3(-12.5f,0,0);
-            obj.transform.Translate(vec * Time.deltaTime, Space.World);
-
-            // obj.transform.position = new Vector3(obj.transform.position.x-12f*Time.deltaTime,0,0);
-
-            // Debug.Log(obj.transform.position.x);
         }
-
     }
 }
