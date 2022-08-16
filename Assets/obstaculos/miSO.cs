7512f6b5a15e1f8d664f55e16ff3def514a32a12
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-08-16 16:07:28 -0300

    Un poco de limpieza y documentación.

--- Assets/obstaculos/miSO.cs
+++ Assets/obstaculos/miSO.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "miSoObstaculos", menuName = "ScriptableObjects/miSO", order = 1)]
 public class miSO : ScriptableObject
 {
-
-
-	public List<List<GameObject>> listaObstaculos = new List<List<GameObject>>();
-	private List<GameObject> obsChicos = new List<GameObject>();
-	private List<GameObject> obsGrandes = new List<GameObject>();
+	public List<List<GameObject>> listaObstaculos = new List<List<GameObject>>(); // Lista de lista de obstáctulos para varian entre grandes y chicos.
+	private List<GameObject> obsChicos = new List<GameObject>(); // Lista de los obstáculos chicos.
+	private List<GameObject> obsGrandes = new List<GameObject>(); // Lista de los obstáculos grandes.
 
     public void Awake()
     {
-    	if (listaObstaculos.Count == 0)
+    	if (listaObstaculos.Count == 0) // Si la lista de obstáculos está vacía cargar los recursos.
     	{
+    		// Carga los recursos que están en el editor y los agrega a su respectiva tabla de chico ó grande y luego a la lista de listas.
+    		// ↓ Obs grandes.
 			var carga_obs_grandes = Resources.LoadAll<GameObject>("Targets/grandes");
 			for (int i = 0; i < carga_obs_grandes.Length; i++) 
 			{
@@ -22,6 +22,7 @@ public class miSO : ScriptableObject
 			}
 			listaObstaculos.Add(obsGrandes);
 
+			// ↓ Obs chicos.
 			var carga_obs_chicos = Resources.LoadAll<GameObject>("Targets/chicos");
 			for (int i = 0; i < carga_obs_chicos.Length; i++) 
 			{
@@ -29,8 +30,7 @@ public class miSO : ScriptableObject
 			}
 			listaObstaculos.Add(obsChicos);
 			
-
-	        Debug.Log("++ " + carga_obs_chicos.Length + " ++");
+	        // Debug.Log("++ " + carga_obs_chicos.Length + " ++");
 	    }
     }
 }
