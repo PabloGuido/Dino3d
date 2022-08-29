593455b92e0d749e68677631eadac716c252b7a0
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-08-29 14:58:51 -0300

    Resolviendo colisiones va funcionando.

--- Assets/scripts/playerColision.cs
+++ Assets/scripts/playerColision.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 public class playerColision : MonoBehaviour
 {
     public playerScr objectWithScript;
-    void OnCollisionEnter(Collision collisionInfo)
+    // void OnCollisionEnter(Collision collisionInfo)
+    // {
+    //     //Output the Collider's GameObject's name
+    //     Debug.Log(collisionInfo.gameObject.tag);
+
+    // }
+    void FixedUpdate()
     {
-        //Output the Collider's GameObject's name
-        // Debug.Log(collisionInfo.gameObject.tag);
-        Debug.Log(objectWithScript.gravity);
+
     }
 
-
+        void OnTriggerEnter(Collider collisionInfo)
+        {
+        Debug.Log(collisionInfo.gameObject.tag);
+        }
 
 }
