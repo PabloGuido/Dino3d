4689d505a8284a95093e2119e25032508cbd61d9
Pablo Sonaglioni <pablosonaglioni@gmail.com>
2022-08-30 11:04:48 -0300

    Nuevo script de Piso.

---
+++ Assets/scripts/Piso.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piso : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> objetos;
+    public miSO miSOScript;
+
+    GameObject piso01;
+    GameObject piso02;
+
+    public Datos datos;
+    void Start()
+    {
+        miSOScript = (miSO)ScriptableObject.CreateInstance(typeof(miSO));
+        piso01 = GameObject.Find("piso01");
+        piso02 = GameObject.Find("piso02");
+
+        objetos.Add(piso01);
+        objetos.Add(piso02);
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!datos.game_over)
+        {
+            foreach(GameObject obj in objetos)
+            {
+
+                if (obj.transform.position.x <= -40){
+                    // obj.transform.position = new Vector3(40f,0,0);
+                    if (obj.name == "piso01"){
+                        obj.transform.position = new Vector3(piso02.transform.position.x+40f ,-0.4f,0);
+                        obj.GetComponent<pisoInstanciador>().refrescarObstaculo();
+                        // test.funcionDePrueba();
+                        return;
+                    }
+                    else{
+                        obj.transform.position = new Vector3(piso01.transform.position.x+40f ,-0.4f,0);
+                        // obj.funcionDePrueba();
+                        obj.GetComponent<pisoInstanciador>().refrescarObstaculo();
+                        return;
+                    }
+                }
+                var vec = new Vector3(-12.5f,0,0);
+                obj.transform.Translate(vec * Time.deltaTime, Space.World);
+                // Antes estaba usando solo el transform y moviendo pero se cambió a Translate.
+            }
+        }
+    }
+}
